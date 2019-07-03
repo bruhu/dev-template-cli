0e38174166134e7413e5e073ea2d5581ade6c319
Bru <annabru.abh@gmail.com>
2019-07-03 16:41:38 +0200

    folderList prints correctly folder names from templates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //these are the module imports, this comment makes us VERY happy :D
 const path = require("path");
 const { showHelp } = require("./messaging");
+const { folderList } = require("./printFolderNames");
 
 const args = process.argv.slice(2, process.argv.length);
 if (args.includes("--help")) {
@@ -8,34 +9,34 @@ if (args.includes("--help")) {
 }
 
 if (args.includes("ls") && args.includes("templates")) {
-  
+  folderList();
 }
 
 //console.log(__dirname);
 
-const printFolderNames = path => {
-  console.log("whatever");
-};
+// const printFolderNames = path => {
+//   console.log("whatever");
+// };
 
-const createNewTemplate = (type, name, dest) => {
-  console.log(type, name, dest);
-};
+// const createNewTemplate = (type, name, dest) => {
+//   console.log(type, name, dest);
+// };
 
 // if arguments contains ls and templates
 // read all folders in templates and print them out
-if (args[0] == "ls" && args[1] == "templates") {
-  const PATH = path.join(__dirname + "/templates");
-  console.log(PATH);
-  printFolderNames(PATH);
-} else if (args[0] == "create") {
-  //type of template
-  const type = args[1];
-  const name = args[2];
-  const dest = args[3];
-  createNewTemplate(type, name, dest);
-} else {
-  console.log("nope for now ;D");
-}
+// if (args[0] == "ls" && args[1] == "templates") {
+//   const PATH = path.join(__dirname + "/templates");
+//   console.log(PATH);
+//   printFolderNames(PATH);
+// } else if (args[0] == "create") {
+//   //type of template
+//   const type = args[1];
+//   const name = args[2];
+//   const dest = args[3];
+//   createNewTemplate(type, name, dest);
+// } else {
+//   console.log("nope for now ;D");
+// }
 
 // create node-cli —name="my-new-cli" dest=~/Projects/cli/
 // if create and node-cli and -name and dest
--- printFolderNames.js
+++ printFolderNames.js
@@ -4,11 +4,10 @@ const fs = require("fs");
 
 exports.folderList = () => {
   const templatesFolder = "./templates";
-  fs.readdir(templatesFolder).forEach(folder => {
+  fs.readdirSync(templatesFolder).forEach(folder => {
     console.log(folder);
   });
 };
-
 
 //source:
 //https://stackoverflow.com/questions/2727167/how-do-you-get-a-list-of-the-names-of-all-files-present-in-a-directory-in-node-j#2727191
